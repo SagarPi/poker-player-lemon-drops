4724d4f0d9f07a26a857be4eed56675cf2e4ad05
SagarPi <11king.sagar@gmail.com>
2023-06-08 14:06:13 -0400

    Fix unit test

--- src/__tests__/unit.test.ts
+++ src/__tests__/unit.test.ts
@@ -1,12 +1,28 @@
 import PlayerService from "../PlayerService";
+import {Card, GameState, Player} from "../types/GameState";
 
 describe("test", () => {
     it("server never ", () => {
         const betCallback = jest.fn();
         const minRaise = 10;
-        // @ts-ignore
-        // new PlayerService().betRequest({minimum_raise: minRaise}, betCallback);
-        // expect(betCallback).toHaveBeenCalledTimes(1)
-        // expect(betCallback).toHaveBeenCalledWith(minRaise)
+        const mockGameState: GameState = {
+                tournament_id: "string",
+                game_id: "string",
+                round: 1,
+                players: [],
+                small_blind: 1,
+                big_blind: 1,
+                orbits: 1,
+                dealer: 1,
+                community_cards: [],
+                current_buy_in: 1,
+                pot: 1,
+                in_action: 1,
+                minimum_raise: 1,
+                bet_index: 1,
+            };
+        new PlayerService().betRequest({...mockGameState, minimum_raise: minRaise}, betCallback);
+        expect(betCallback).toHaveBeenCalledTimes(1)
+        expect(betCallback).toHaveBeenCalledWith(minRaise)
     });
 })
