895c86ffe690fb9955ab68b50c5fd8e81636a54b
Prafulla Pol <pol@rushstreetinteractive.com>
2023-06-08 10:09:33 -0400

    log bet request

--- src/Player.ts
+++ src/Player.ts
@@ -1,10 +1,11 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
+    console.log("LEMON_DROPS: betRequest: ", {gameState});
     betCallback(0);
   }
 
   public showdown(gameState: any): void {
-
+    console.log("LEMON_DROPS: showdown: ", {gameState});
   }
 };
 
