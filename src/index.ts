a8ce013bb06f6ec342d0b13725c840397e6205c5
Prafulla Pol <pol@rushstreetinteractive.com>
2023-06-08 16:22:08 -0400

    Handle bad requests

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,13 @@ app.post('/', (req, res) => {
     console.log("index request body: ", JSON.stringify(req.body));
     const gameState = (typeof req.body.game_state === 'string' || req.body.game_state instanceof String ) ? JSON.parse(req.body.game_state) : req.body.game_state;
     if (req.body.action === 'bet_request') {
-        player.betRequest(gameState, bet => res.status(200).send(bet.toString()));
+        try {
+            player.betRequest(gameState, bet => res.status(200).send(bet.toString()));
+        } catch (error) {
+            
+        } finally {
+            res.status(200).send("5");
+        }
     } else if (req.body.action === 'showdown') {
         player.showdown(JSON.parse(req.body.game_state));
         res.status(200).send('OK');
